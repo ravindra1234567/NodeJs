5d0cd00ebaa486e2a564ca1a2a1c1dbaa76ac782
Ravindra Kumar Kushwaha <47721333+ravindra1234567@users.noreply.github.com>
2020-10-06 00:30:06 +0530

    add readHtml.js and htmlForm.html

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 var http = require("http");
 http.createServer(function(req,res){
-    res.write("hello Ravindra fjhsk");
+    res.write("hello Ravindra fjhsk ravindra");
     res.end();
 }).listen(5000);
